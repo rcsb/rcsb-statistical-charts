bbe84b652954e44a95254467cf83a77c3a28ba90
Zintis May-Krumins <zintis.may@rcsb.org>
2023-09-06 10:11:53 -0400

    Buttons and barClickCallback attempt

--- src/StatsApp/StatsApp.tsx
+++ src/StatsApp/StatsApp.tsx
@@ -18,41 +18,56 @@ export function StatsApp(){
     if (!mainFacet.facet || !mainFacet.chartType) return null;
 
     return (
-        <div className="StatsApp Component">
+        <div className="StatsApp Component container" ref={chartRef}>
             <div style={{marginBottom:20}}>
                 {/* Select 1st Dataset */}
-                <label>1st Dimension</label>
-                <select onChange={(e)=>setOpt1(Number(e.target.value))}>
-                    {
-                        FACET_STORE.map((facet, index)=>{
-                            return <option key={index} value={index}>{facet.facetName}</option>
-                        })
-                    }
-                </select>
+                <div className="row mb-1">
+                    <div className="btn-group">
+                        <div className={`btn`}>1st Dimension</div>
+                        {
+                            FACET_STORE.map((facet, index)=>{
+                                let buttonClass = index === opt1 ? 'btn-primary' : 'btn-light'
+                                return <div className={`btn ${buttonClass}`} onClick={()=>setOpt1(index)} key={index}>{facet.facetName}</div>
+                            })
+                        }
+                    </div>
+                </div>
                 {/* Select 2nd Dataset */}
-                <label>2nd Dimension</label>
-                <select onChange={(e)=>setOpt2(Number(e.target.value))}>
-                    {
-                        ADDITIONAL_FACET_STORE.map((facet, index)=>{
-                            return <option key={index} value={index}>{facet.facetName}</option>
-                        })
-                    }
-                </select>
+                <div className="row">
+                    <div className="btn-group">
+                        <div className={`btn`}>2nd Dimension</div>
+                        {
+                            ADDITIONAL_FACET_STORE.map((facet, index)=>{
+                                let buttonClass = index === opt2 ? 'btn-primary' : 'btn-light'
+                                return <div className={`btn ${buttonClass}`} onClick={()=>setOpt2(index)} key={index}>{facet.facetName}</div>
+                            })
+                        }
+                    </div>
+                </div>
                 <span><button onClick={() => {saveTargetAsImage(chartRef.current, CHART_FILE_NAME)}}>Save</button> Chart as Image</span>
                 
             </div>
             {/* Draw the Chart */}
-            <div ref={chartRef}>
-                <FacetPlot
-                    firstDim={mainFacet.facet}
-                    secondDim={
-                        mainFacet.facetId != additionalFacet?.facetId ? additionalFacet?.facet : undefined
-                    }
-                    chartType={mainFacet.chartType}
-                    returnType={ReturnType.Entry}
-                    chartConfig={mainFacet.chartConfig}
-                />   
-            </div>
+            <FacetPlot
+                firstDim={mainFacet.facet}
+                secondDim={
+                    mainFacet.facetId != additionalFacet?.facetId ? additionalFacet?.facet : undefined
+                }
+                chartType={mainFacet.chartType}
+                returnType={ReturnType.Entry}
+                chartConfig={{
+                    ...mainFacet.chartConfig,
+                    // @#@#@# sort of works, errors when clicking on background :(
+                    // barClickCallback
+                }}
+            />
         </div>
     )
 }
+
+// @#@#@# not working correctly yet
+// function barClickCallback(datum:any, data:any, event:any){
+//     try{
+//         console.log("barClickCallback", datum, data, event)
+//     }catch(e){console.log("error", e)}
+// }
