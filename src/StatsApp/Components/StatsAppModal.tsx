9a82fbad41c2c01b125cf3220b670319bea5a319
Zintis May-Krumins <zintis.may@rcsb.org>
2023-10-20 16:58:15 -0400

    Added Modal

---
+++ src/StatsApp/Components/StatsAppModal.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+// import Button from 'react-bootstrap/Button';
+import {Modal} from 'react-bootstrap';
+
+function StatsAppModal(props:any) {
+  return (
+    <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+      <Modal show={props.show} onHide={props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.children}</Modal.Body>
+        <Modal.Footer>
+          {/* <Button variant="secondary" onClick={props.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.handleClose}>
+            Save Changes
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default StatsAppModal;
